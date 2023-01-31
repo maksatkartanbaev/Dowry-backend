00654e6a596c44004547f4a86eb5877715f089e3
maksatkartanbaev <kartamaks@gmail.com>
2023-01-31 09:10:48 +0600

    added ability to retrieve education and place lists for convenient list in frontend

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import DowryView
+from .views import DowryView, EducationList, PlaceList
 
 urlpatterns = [
     path('dowry/', DowryView.as_view(), name='dowry'),
+    path('education/', EducationList.as_view(), name='education-list'),
+    path('places/', PlaceList.as_view(), name='place-list'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,12 @@
 from rest_framework.views import APIView
+from django.http import JsonResponse
 from rest_framework.response import Response
 from .models import AgeValues, EducationValues, PlaceValues, WeightValues
 
 
 class DowryView(APIView):
-    def post(self, request):
+    @staticmethod
+    def post(request):
         age = request.data.get('age')
         age_weight = AgeValues.objects.get(start_age__lte=age, end_age__gte=age)
         weight = request.data.get('weight')
@@ -15,3 +17,19 @@ class DowryView(APIView):
         price = place_of_birth_weight.value * age_weight.value * weight_weight.value * education_weight.value
 
         return Response({'price': price})
+
+
+class EducationList(APIView):
+    @staticmethod
+    def get(request, *args, **kwargs):
+        educations = EducationValues.objects.all()
+        data = {'educations': [{"name": education.education} for education in educations]}
+        return JsonResponse(data, safe=False)
+
+
+class PlaceList(APIView):
+    @staticmethod
+    def get(request, *args, **kwargs):
+        places = PlaceValues.objects.all()
+        data = {'places': [{"name": place.place} for place in places]}
+        return JsonResponse(data, safe=False)
